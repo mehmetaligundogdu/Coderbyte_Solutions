e3efb3a80e2cfaead0e3afd94ab4e90fdc792432
kodluyoruz <gundogdu.102@outlook.com>
2023-06-10 19:07:16 +0300

    First Factorial-solution

--- easy.js
+++ easy.js
@@ -1,6 +1,8 @@
 import { userValidation } from "./Easy/userValidation.js";
-import {longestWord} from "./Easy/longestWord.js";
-
-
 // console.log(userValidation("u__hello_world123"));
-console.log(longestWord('fun!! &time **deneme123'));
+
+import {longestWord} from "./Easy/longestWord.js";
+// console.log(longestWord('fun!! &time **deneme123'));
+
+import {firstFactorial} from "./Easy/firstFactorial.js";
+console.log(firstFactorial(5));
